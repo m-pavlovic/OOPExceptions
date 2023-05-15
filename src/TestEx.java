5a133ba75dde8f552d349627bbbae642790df9ba
m-pavlovic <marija.pavlovic19@gmail.com>
2023-05-15 16:30:35 +0200

    IO exceptions prvi dio

---
+++ src/TestEx.java
@@ -0,0 +1,27 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class TestEx {
+
+
+    private static Scanner scanner;
+
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, -11, 25, 99};
+  //      System.out.println("Element: " + arr[9]);
+        String filePath = "src/dataFile.txt";
+        readFile(filePath);
+    }
+
+
+    private static void readFile(String filePath) {
+        File file = new File(filePath);
+        try {
+            scanner = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found --> " + file.getName());
+        }
+    }
+}
