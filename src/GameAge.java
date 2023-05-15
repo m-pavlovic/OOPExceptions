dbf08a5d60074da73e637a5ba6b2d35e908bb574
m-pavlovic <marija.pavlovic19@gmail.com>
2023-05-15 18:37:31 +0200

    IO exceptions 5 dio - gameage zadnje

--- src/GameAge.java
+++ src/GameAge.java
@@ -1,19 +1,49 @@
 import javax.management.InvalidAttributeValueException;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class GameAge {
 
     private static Scanner scanner;
+    private static ArrayList<Integer> ages = new ArrayList<>();
 
     public static void main(String[] args) {
         scanner = new Scanner(System.in);
-        System.out.println("Enter your age: ");
-        int age = scanner.nextInt();
-        try {
-            checkAge(age);
-        } catch (InvalidAttributeValueException e) {
-            System.out.println("Exception message: " + e.getMessage());
+        ages = new ArrayList<>();
+        enterAge();
+    }
+
+    private static void enterAge() {
+        boolean status = true;
+        while (status) {
+            System.out.println("Enter your age: ");
+            int age = 0;
+            try {
+                age = scanner.nextInt();
+                checkAge(age);
+                ages.add(age);
+            } catch (InputMismatchException ime) {
+                System.out.println("You entered something that is not an integer");
+                String scannerOld = scanner.next();
+                System.out.println("Memory of the scanner before clearing: " + scannerOld);
+            } catch (InvalidAttributeValueException e) {
+                System.out.println(e.getMessage());
+                scanner.nextLine();
+            } finally {
+                System.out.println("For new input press y, for exiting the program press any key");
+                String cont = scanner.next();
+                if (cont.equalsIgnoreCase("y")) {
+                    System.out.println("Continuing...");
+                } else {
+                    System.out.println("Exiting...");
+                    status = false;
+                }
+            }
         }
+        calculateAverage(ages);
+
+
     }
 
     private static void checkAge(int age) throws InvalidAttributeValueException {
@@ -23,5 +53,15 @@ public class GameAge {
         } else {
             System.out.println("Age is correct and is: " + age);
         }
+    }
+
+    private static void calculateAverage(ArrayList<Integer> ages) {
+        int sum = 0;
+        float avg;
+        for (int age : ages) {
+            sum += age;
+        }
+        avg = (float) sum / ages.size();
+        System.out.println("Average age is: " + avg);
     }
 }
