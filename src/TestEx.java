88624fd59b61ae507431270d84a78568dc5a21cc
m-pavlovic <marija.pavlovic19@gmail.com>
2023-05-15 16:36:27 +0200

    IO exceptions prvi dio

--- src/TestEx.java
+++ src/TestEx.java
@@ -20,6 +20,10 @@ public class TestEx {
         File file = new File(filePath);
         try {
             scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                System.out.println(line);
+            }
         } catch (FileNotFoundException e) {
             System.out.println("File not found --> " + file.getName());
         }
